777aab16d002fc821c42257f16a86514ebcab0c3
Your Name <you@example.com>
2020-01-27 14:12:47 -0800

    Initial commit

---
+++ app/src/main/java/com/example/lab2_api19_v4/Search.java
@@ -0,0 +1,27 @@
+package com.example.lab2_api19_v4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Search extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+    public void searchCaption(View view) {
+
+    }
+
+    public void searchTime(View view) {
+
+    }
+
+    public void searchLocation(View view) {
+
+    }
+    }
+
